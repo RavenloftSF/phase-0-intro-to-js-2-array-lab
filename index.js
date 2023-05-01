2927d3c623eb40c91dba7111e012b51ea6e8ad6d
JosephBorrelli <josephborrelli80@gmail.com>
2023-05-01 13:27:48 -0400

    broke on through to the other side

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    console.log(cats)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    console.log(cats)
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    console.log(cats)
+}
+function appendCat (name) {
+    return [...cats, name];
+}
+function prependCat (name) {
+    return [name, ...cats];
+}
+
+function removeLastCat () {
+    let copyOfCats = [...cats]
+    console.log("copyOfCats", copyOfCats)
+    copyOfCats.pop()
+    return copyOfCats
+}
+
+function removeFirstCat () {
+    return cats.slice(1)
+}
